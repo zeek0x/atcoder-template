5b313a2055bd9c4451a29f152a5deb06602955ca
zeek0x <0h23k1.n40k1@gmail.com>
2021-05-09 04:53:57 +0900

    erl: No bind macro.

--- Main.erl
+++ Main.erl
@@ -1,27 +1,17 @@
 -module('Main').
 -export([main/1]).
 
--define(OUT(__Y__),
-    begin
-        __X__ = __Y__,
-        case is_list(__X__) orelse is_binary(__X__) of
-            true -> io:format("~s~n", [__X__]);
-            false -> io:format("~p~n", [__X__])
-        end
-    end).
--define(OUT_L(__L__, __Sep__),
-    begin
-        __Pat__ = lists:concat(lists:join(__Sep__, lists:duplicate(length(__L__), "~p"))),
-        io:format(__Pat__ ++ "~n", __L__)
-    end).
--define(OUT_L(__L__), ?OUT_L(__L__, " ")).
+-define(OUT(P), io:format("~p~n", [P])).
+-define(OUT_S(S), io:format("~s~n", [S])).
+-define(OUT_L(L, Sep), io:format(lists:concat(lists:join(Sep, lists:duplicate(length(L), "~p"))) ++ "~n", L)).
+-define(OUT_L(L), ?OUT_L(L, " ")).
 
 solve() ->
     [S] = in("~s"),
     % [N] = in("~d"),
     % Pat = "~d" ++ lists:append(lists:duplicate(N-1, " ~d")),
     % L = in(Pat),
-    ?OUT(S),
+    ?OUT_S(S),
     ok.
 
 main(_) ->
